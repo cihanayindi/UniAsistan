5a64c1b50684f51516beab440551d02ca5d23fd5
Cihan Ayindi <cihan_ayindi@hotmail.com>
2025-06-13 18:26:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -41,10 +41,6 @@ app = FastAPI(
 
 origins = [
     "https://uni-asistan.vercel.app",  # Vercel adresiniz
-    "http://localhost",
-    "http://localhost:8000",
-    "http://127.0.0.1",
-    "http://127.0.0.1:8000",
     # Gerekirse başka adresler de eklenebilir
 ]
 
